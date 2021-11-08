af003921b29d40b83db5504f119f24e48995018f
sajidhasan <md.sajidhasan@yahoo.com>
2021-11-08 19:05:14 +0000

    exp001-MSc-CS-UoG

--- ImagePickerExp/ViewController.swift
+++ ImagePickerExp/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var selectImageButton: UIButton!
+    
+    
+    var imagePicker: UIImagePickerController = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureImagePicker()
     }
+    
+    @IBAction func selectImageButtonPressed(_ sender: UIButton) {
+        pickImage()
+        print("Select Image button pressed")
+    }
+    
+    
+    fileprivate func configureImagePicker(){
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        //imagePicker.allowsEditing = false
+        imagePicker.mediaTypes = ["public.image"]
+    }
+    fileprivate func pickImage(){
+        present(imagePicker, animated: true)
+    }
+    
 
 
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else
+        {
+            fatalError("error: no photo picked")
+        }
+        
+        picker.dismiss(animated: true) {
+            self.imageView.image = selectedImage
+        }
+        print("UIImagePickerControllerDelegate -> imagePickerController called")
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true) {
+            print("UIImagePickerController: dismissed")
+        }
+        
+    }
 }
 
